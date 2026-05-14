e65a4a3f33ca5b62a46f407da2efeee66148b331
Krasi999 <kgushmakov@tu-sofia.bg>
2026-05-14 19:21:58 +0300

    AddSomething

---
+++ MVVM example/MVVM example/MinimalMVVM/View/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MinimalMVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MinimalMVVM.View
+{
+    public partial class MainWindow : Window
+    {
+        private readonly Presenter _upperVM = new Presenter();
+        private readonly LowerPresenter _lowerVM = new LowerPresenter();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = _upperVM;
+        }
+
+        private void SwitchViewModel_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as System.Windows.Controls.Button;
+
+            if (this.DataContext is Presenter)
+            {
+                this.DataContext = _lowerVM;
+                button.Content = "Switch to UPPERCASE Mode";
+            }
+            else
+            {
+                this.DataContext = _upperVM;
+                button.Content = "Switch to LowerCase Mode";
+            }
+        }
+    }
+}
---
+++ MVVM example/MVVM example/MinimalMVVM/ViewModel/LowercasePresenter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using MinimalMVVM.Model;
+
+namespace MinimalMVVM.ViewModel
+{
+    public class LowerPresenter : ObservableObject
+    {
+        private readonly TextConverter _textConverter = new TextConverter(s => s.ToLower());
+        private string _someText;
+        private readonly ObservableCollection<string> _history = new ObservableCollection<string>();
+
+        public string SomeText
+        {
+            get { return _someText; }
+            set
+            {
+                _someText = value;
+                RaisePropertyChangedEvent("SomeText");
+            }
+        }
+
+        public IEnumerable<string> History => _history;
+
+        public ICommand ConvertTextCommand => new DelegateCommand(ConvertText);
+
+        private void ConvertText()
+        {
+            if (string.IsNullOrWhiteSpace(SomeText)) return;
+
+            AddToHistory(_textConverter.ConvertText(SomeText));
+            SomeText = String.Empty;
+        }
+
+        private void AddToHistory(string item)
+        {
+            if (!_history.Contains(item))
+                _history.Add(item);
+        }
+    }
+}
